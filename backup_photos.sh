017b047751da78f22dc6921be7f0d7566de1a1e6
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2025-07-15 13:32:27 +0100

    Update backup_photos.sh

--- backup_photos.sh
+++ backup_photos.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# you need to have exiftool installed on your system!
 
-find /Users/aa3025/Pictures/Photos\ Library.photoslibrary/originals -maxdepth 2 -mindepth 2 -type f -exec /Users/aa3025/Pictures/process_photos.sh {} \;
+user=$USER
+
+# copy from:
+src_folder="/Volumes/$USER/Pictures/Photos Library/"
+# copy to:
+destination_folder=/Volumes/my_backup_drive/photos
+
+find /Users/aa3025/Pictures/Photos\ Library.photoslibrary/originals -maxdepth 2 -mindepth 2 -type f -exec /Users/aa3025/Pictures/process_photos.sh {} "src_folder" "$destination_folder" \;
 
